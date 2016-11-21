c80a8fbcb40ee47d3c3159edb62583d08fec7729
Marc Wilson <marcw@silverfern.io>
2016-11-21 05:24:44 +0000

    Bump version.

--- iso8601utils/__init__.py
+++ iso8601utils/__init__.py
@@ -2,4 +2,4 @@ from iso8601utils.interval import interval
 from iso8601utils.duration import duration
 
 __all__ = ['interval', 'duration', 'parsers', 'validators']
-__version__='0.1.1'
+__version__='0.1.2'
